b0ac4d39133df51f35d8d6d011d9b561626b14ac
rsheikh <red.sheikh@gmail.com>
2024-08-15 23:41:00 +0100

    test: implement tests for InstructionParser class and methods

--- src/test/java/org/example/input/Parser/InstructionParserTest.java
+++ src/test/java/org/example/input/Parser/InstructionParserTest.java
@@ -1,16 +1,61 @@
 package org.example.input.Parser;
 
+import org.example.input.Instruction;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class InstructionParserTest {
 
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+
     @Test
-    void getRoverInstructions() {
+    @DisplayName("Test getRoverInstructions with Valid Input")
+    void getRoverInstructionsWithValidInputTest() {
+        String simulatedInput = "LLRM";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(simulatedInput.getBytes());
+        System.setIn(inputStream);
+
+        String returnedResult = InstructionParser.getRoverInstructions();
+
+        assertEquals(simulatedInput, returnedResult);
     }
 
     @Test
-    void parseInstructions() {
+    @DisplayName("Test getRoverInstructions with Invalid Input")
+    void getRoverInstructionsWithInvalidInputTest() {
+        String simulatedInput = "ABC";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(simulatedInput.getBytes());
+        System.setIn(inputStream);
+        System.setOut(new PrintStream(out)); //console's output
+
+        String errorMessage = "Enter your instructions for the Rover: \n" +
+                "Instructions can only contain L, R or M only. Try again";
+
+        assertThrows(NoSuchElementException.class, InstructionParser::getRoverInstructions);
+        assertEquals(errorMessage.trim(), out.toString().trim());
+    }
+
+    @Test
+    @DisplayName("Test parseInstructions to create an ArrayList of Instructions")
+    void parseInstructionsTest() {
+        String input = "LRMM";
+        ArrayList<Instruction> expectedList = new ArrayList<>();
+        expectedList.add(Instruction.L);
+        expectedList.add(Instruction.R);
+        expectedList.add(Instruction.M);
+        expectedList.add(Instruction.M);
+
+        ArrayList<Instruction> instructionsList = InstructionParser.parseInstructions(input);
+
+        assertEquals(expectedList, instructionsList);
+        assertEquals(4, instructionsList.size());
     }
 }
