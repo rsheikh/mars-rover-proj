c1cd0dad35db41bc38da23d9d69130fa20f4e923
rsheikh <red.sheikh@gmail.com>
2024-08-15 11:56:53 +0100

    feat: Modified InstructionParser class to obtain plateau size and landing position input from user

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.input.Parser.InstructionParser;
+import org.example.input.PlateauSize;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/org/example/input/Parser/InstructionParser.java
+++ src/main/java/org/example/input/Parser/InstructionParser.java
@@ -2,39 +2,46 @@ package org.example.input.Parser;
 
 import org.example.input.Instruction;
 
-import java.util.InputMismatchException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InstructionParser {
 
-    public static String getRoverInstructions() {
+    public static void getRoverInstructions() {
         Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> roverInstructions = new ArrayList<>(3);
+        String plateauSize = "";
+        String startPosition = "";
         String instructions = "";
         String regexMatch = Instruction.L.name() + Instruction.R.name() + Instruction.M.name();
-        System.out.println(regexMatch);
 
-//        try {
-            System.out.println("Enter your instructions for the Rover: ");
+        System.out.println("Enter size of the Plateau (x,y):");
+        plateauSize = scanner.next();
+        roverInstructions.add(plateauSize);
+
+        System.out.println("Enter your landing position and facing direction (x, y, direction):");
+        startPosition = scanner.next();
+        roverInstructions.add(startPosition);
+
+        System.out.println("Enter your instructions for the Rover: ");
+        instructions = scanner.next();
+        if(instructions.matches("^["+ regexMatch+"]+$")) {
+            System.out.println("Yay good entry: " + instructions);
+        } else {
+            System.out.println("Instructions can only contain L, R or M only. Try again");
             instructions = scanner.next();
-            if(instructions.matches("^["+ regexMatch+"]+$")) {
-                System.out.println("Yay good entry: " + instructions);
-            } else {
-                System.out.println("Instructions can only contain L, R or M only. Try again");
-                instructions = scanner.next();
-            }
+        }
+        roverInstructions.add(instructions);
 
-//        } catch (InputMismatchException e) {
-//            System.out.println("Invalid Input. Instructions can only contain L, R or M only. Try again.");
-//            scanner.nextLine();
-//        }
-        System.out.println("Instructions are: " + instructions);
-        parseInstructions(instructions);
-        return "";
+        System.out.println("Rover Instructions are: " + roverInstructions);
+        parseInstructions(roverInstructions);
     }
 
-    public static void parseInstructions(String instructions) {
+    public static void parseInstructions(ArrayList<String> instructions) {
+        System.out.println("In parse method");
 
-
+        //should return the final position - then elsewhere we do other things
     }
 
 }
